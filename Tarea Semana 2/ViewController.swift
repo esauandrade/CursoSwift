7816e45c9bb324c3c8754803378ad68a49a902ed
Esau Sebastian Andrade Ortiz <esau@macbook-pro-de-esau.local>
2016-10-16 23:37:45 -0500

    Initial Commit

---
+++ Tarea Semana 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tarea Semana 2
+//
+//  Created by Esau Sebastian Andrade Ortiz on 16/10/16.
+//  Copyright © 2016 Esau Sebastian Andrade Ortiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
